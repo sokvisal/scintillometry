1d9b77ceafd4b32c986d8f1f0933bfb9e617ac72
sokvisal <visal.lesok@gmail.com>
2017-05-01 10:43:14 -0400

    ploting utilities

---
+++ toeplitz_decomp/plot_simulated.py
@@ -0,0 +1,135 @@
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pylab as plt
+import matplotlib.cm as cmaps
+from scipy.fftpack import fftshift, fft2, ifft2, ifftshift
+import re
+import os.path
+import glob
+from toeplitz_scint import *
+
+matplotlib.rcParams.update({'font.size': 13})
+
+n = str(sys.argv[1])
+m=int(sys.argv[2]) # frequency
+offsetn=int(sys.argv[3]) # time
+offsetm=int(sys.argv[4]) # offset in freq
+
+n = 21
+m = 21
+offsetn = 0
+offsetm = 0
+
+if m%2 == 0:
+    xticks = np.arange(-m/2+0.5, m/2+1, 2)
+    xtickslabels= np.arange(-m/2, m/2+1, 2)
+    yticks = np.arange(-n/2+0.5, n/2+1, 2)
+    ytickslabels= np.arange(-n/2, n/2+1, 2)
+else:
+    xticks = np.arange(-m/2+0.5, m/2+1, 2)
+    xtickslabels= np.arange(-m/2+1, m/2+1, 2)
+    yticks = np.arange(-n/2+0.5, n/2+1, 2)
+    ytickslabels= np.arange(-n/2+1, n/2+1, 2)
+extent = [-n/2, n/2, -m/2, m/2]
+
+
+retrieved = (reconstruct(21, 42, 0, 0))
+
+plt.figure(figsize=(18,12))
+plt.subplot(2,3,1)
+im=plt.imshow(retrieved.real, interpolation='nearest', origin='lower', 
+              cmap='viridis', extent = extent) 
+plt.title(r"Retrieved RE $\tilde{E}(\tau, f_\nu)$", color='white')
+plt.xlabel(r"$f_\nu$")
+plt.ylabel(r"$\tau$")
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+plt.subplot(2,3,2)
+im=plt.imshow(retrieved.imag, interpolation='nearest', origin='lower', 
+              cmap='viridis',  extent = extent)
+plt.title(r"Retrieved IM $\tilde{E}(t, \nu)$", color='white')
+plt.xlabel(r"$f_\nu$")
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+e = np.load('e_field_fourier_nonoise_tfd.npy')
+         
+plt.subplot(2,3,4)
+plt.imshow(e.real, extent = extent, origin='lower',cmap='viridis', interpolation='nearest')
+plt.title(r"Simulated RE $\tilde{E}(\tau,f_D)$", color='white')
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+plt.subplot(2,3,5)
+plt.imshow(e.imag, extent = extent, origin='lower',cmap='viridis', interpolation='nearest')
+plt.title(r"Simulated RE $\tilde{E}(\tau,f_D)$", color='white')
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+retrieved = ifft2(ifftshift(retrieved))*retrieved.size
+e = ifft2(ifftshift(e))*e.size
+         
+plt.subplot(2,3,3)
+plt.imshow(np.angle(retrieved), extent = extent, origin='lower',cmap='viridis', interpolation='nearest')
+plt.title(r"Retrieved Phase ${E}(f,\nu)$", color='white')
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+plt.subplot(2,3,6)
+plt.imshow(np.angle(e), extent = extent, origin='lower',cmap='viridis', interpolation='nearest')
+plt.title(r"Simulated Phase ${E}(f,\nu)$", color='white')
+ax = plt.gca()
+ax.set_xticks(xticks)
+ax.set_xticklabels(xtickslabels)
+ax.set_yticks(yticks)
+ax.set_yticklabels(ytickslabels)
+
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+
+#plt.savefig('retreieved_offset_freq_domain.png', transparent=True, bbox_inches='tight')
---
+++ toeplitz_decomp/testFactorization_new.py
@@ -0,0 +1,131 @@
+
+import numpy as np
+import os,sys
+from scipy.linalg import cholesky
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pylab as plt
+import matplotlib.cm as cmaps
+
+matplotlib.rcParams.update({'font.size': 13})
+
+##Specifications
+n=21
+m=21
+
+offsetn = 0
+offsetm = 0
+
+pad = 1
+
+##Load the processed data 
+filename = "gate0_numblock_{}_meff_{}_offsetn_{}_offsetm_{}".format(n,m*2, offsetn, offsetm) 
+file = filename + ".dat"
+folder = "processedData"
+
+
+##Load the toepletz file generated from extract_real
+toeplitz1 = np.memmap("{0}/{1}".format(folder,file), dtype='complex', mode='r', shape=(2*m,2*n*m), order='F')
+print toeplitz1.shape
+
+##Creating a square toeplitz matrix
+toeplitz = np.zeros((2*m*n*(1 + pad), 2*n*m*(1 + pad)), complex)
+
+##Gonna be messy, but using toeplitz1 to create toeplitz
+toeplitz[:2*m, :2*n*m] = toeplitz1
+
+for i in range(0,n*(1 + pad)):
+    print i
+    T_temp = toeplitz[:2*m,2*i*m:2*(i+1)*m]
+    print T_temp
+    for j in range(0,n*(1 + pad) - i):
+        k = j + i
+        toeplitz[2*j*m:2*(j+1)*m,2*k*m:2*(k+1)*m] = T_temp
+        toeplitz[2*k*m:2*(k+1)*m, 2*j*m:2*(j+1)*m] = np.conj(T_temp.T)
+
+print toeplitz.shape
+
+print 'begin plot'
+##Plotting thr raw toeplitz matrix
+fig = plt.figure(figsize=(12,6))
+#plt.subplot(1,3,1)
+#print '1'
+#plt.imshow(np.abs(toeplitz))
+#print '2'
+#plt.colorbar()
+#if not pad:
+#        plt.title("Toeplitz matrix with n = {0}, m = {1}".format(n, m*4))
+#else:
+#        plt.title("Zero padded Toeplitz matrix with n = {0}, m = {1}".format(n, m*4))
+##Adding graphing lines
+#ax = fig.gca()
+#ax.set_xticks(np.arange(0,2*n*m*(1 + pad),2*m))
+#ax.set_yticks(np.arange(0,2*n*m*(1 + pad),2*m))
+#ax.grid(True, which='both')
+#if pad:
+#    plt.plot([0, 2*2*n*m], [2*n*m, 2*m*n], '-k')
+#    plt.plot([2*n*m, 2*m*n],[0, 2*2*n*m], '-k')
+#print 'finish'
+#
+###Loading the factorized matrix 
+folder = "gate0_numblock_{}_meff_{}_offsetn_{}_offsetm_{}".format(n, m*2, offsetn, offsetm)
+
+L = np.zeros((2*m*n*(1 + pad),2*m*n*(1 + pad)), complex)
+for i in range(n*(1 + pad)):
+    for j in range(n*(1 + pad)): 
+        path = "results/{0}/L_{1}-{2}.npy".format(folder, i,j)     
+        
+        if os.path.isfile(path):
+            Ltemp = np.load(path)
+            if j == 0 and i == 0:
+                print Ltemp.shape
+            L[2*m*j: 2*m*(j + 1), 2*m*i:2*m*(i + 1)] = Ltemp
+#            
+##The  factorized matrix using Numpy's Cholesky
+#npL = cholesky(toeplitz, True)
+
+print 'begin plot 2'
+plt.subplot(1,2,2)
+
+##Graphical lines
+ax = fig.gca()
+ax.set_xticks(np.arange(0,2*n*m*(1 + pad),4*m))
+ax.set_xticklabels(np.arange(0,2*n*m*(1 + pad),4*m) , rotation='vertical')
+ax.set_yticks(np.arange(0,2*n*m*(1 + pad),4*m))
+ax.grid(True, which='both')
+if pad:
+    plt.plot([0, 2*2*n*m], [2*n*m, 2*m*n], '-k')
+    plt.plot([2*n*m, 2*m*n],[0, 2*2*n*m], '-k')
+
+##Plotting the error difference between our raw and our results multiplied by its transposed conjugate
+#toeplitz=np.where(toeplitz > 1e-6, toeplitz, 1000)
+plt.imshow(np.abs(toeplitz - L.dot(np.conj(L.T))) )
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+plt.title("Errors on the Toeplitz Matrix")
+
+
+plt.subplot(1,2,1)
+##Graphical lines
+ax = fig.gca()
+ax.set_xticks(np.arange(0,2*n*m*(1 + pad),4*m))
+ax.set_xticklabels(np.arange(0,2*n*m*(1 + pad),4*m) , rotation='vertical')
+ax.set_yticks(np.arange(0,2*n*m*(1 + pad),4*m))
+ax.grid(True, which='both')
+if pad:
+        plt.plot([0, 2*2*n*m], [2*n*m, 2*m*n], '-k')
+        plt.plot([2*n*m, 2*m*n],[0, 2*2*n*m], '-k')
+
+##Plotting the factorized matrix
+plt.title("Factorized Toeplitz Matrix")
+plt.imshow(np.abs(L))
+#ax.tick_params(axis='x', colors='white')
+#ax.tick_params(axis='y', colors='white')
+cbar = plt.colorbar()
+#cbar.ax.tick_params(axis='y', colors='white')
+#plt.savefig('padd_toeplitz_err.png', transparent=True, bbox_inches='tight')
+#plt.close
+plt.show()
+
